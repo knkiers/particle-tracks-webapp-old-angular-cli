255b39b1f98e74e912c44ae70f144dc508b89a35
Ken Kiers <knkiers@taylor.edu>
2016-07-04 11:56:23 -0400

    initial commit; app works, including materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import {MaterializeDirective} from "angular2-materialize";
+
+// useful resource for using Materialize components that require js:
+// https://github.com/InfomediaLtd/angular2-materialize/tree/master/app/components
+
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MaterializeDirective]
 })
 export class AppComponent {
   title = 'app works!';
